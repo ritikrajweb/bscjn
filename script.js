0f36fe576ac4f20ac0cb7811d849b4035966e579
ritik-raj-gupta-1 <ritikrajamba12@gmail.com>
2026-03-31 21:15:55 +0530

    Finalized B.Sc. Sem 2 Students Tried Again

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ const studentDB = {
     "Y25102017": { name: "SUHANI", course: "B.Sc.", topics: [5, 13] },
     "Y25105019": { name: "POORNIMA DIXIT", course: "B.Sc.", topics: [6, 18] },
     "Y25105015": { name: "MAMTA NAMDEO", course: "B.Sc.", topics: [7, 12] },
-    "Y25105015": { name: "BINDU SHREE DAS", course: "B.Sc.", topics: [2, 16] },
+    "Y25101002": { name: "BINDU SHREE DAS", course: "B.Sc.", topics: [2, 16] },
 
 
 
